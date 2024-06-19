cff8be28db0f9a61fa7bd9476618fb43720da78a
bhargavsuthar <sutharbhargav1997@gmail.com>
2024-06-19 17:41:35 -0400

    update changes for adding alerts in update functionality

--- Learning/Week6/inc/update.php
+++ Learning/Week6/inc/update.php
@@ -1,5 +1,6 @@
 <?php
     include('../reusable/con.php');
+    include('./functions.php');
     $id = $_POST['id'];
     $schoolName = $_POST['schoolName'];
     $schoolType = $_POST['schoolType'];
@@ -10,10 +11,12 @@
     `School Level` = '". mysqli_real_escape_string($connect, $schoolType)."', `Phone` = '". mysqli_real_escape_string($connect, $phone) ."', `Email` = '". mysqli_real_escape_string($connect, $email)."' WHERE id = '$id'";
     
     $result = mysqli_query($connect, $sql);
+ 
     if($result) {
+        set_messages('School Deleted Successfully', 'success');
         header("Location: ../index.php");
-    }
-    else {
+    } else {
+        set_messages('Something went wrong' , 'error');
         die(mysqli_error($connect));
     }
 
